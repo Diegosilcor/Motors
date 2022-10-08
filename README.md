a2e3d227b74997d3bbe276f8bb9043851e2c7825
Walter Franz <walter.franz@gmail.com>
2022-10-08 18:08:07 -0300

    Agrego WhatApp
    Se inckuye WhapApp y sus funcionalidad. Icono estatico, abre nueva ventana en Chrome con chat de WAP. Agregar numero de cliente ( una vez terminado el depolyment )

--- README.md
+++ README.md
@@ -10,13 +10,13 @@
 
 - [React Router Dom](https://reactrouter.com/en/main "Heading link") Libreria para navegacion y manejos de rutas entre los distintos componentes.
 - [React Spinner](https://www.davidhu.io/react-spinners/ "Heading link") Libreria que permite mostrar al usuario el efecto de carga en la pagina o aplicacion.
-- [React Icons](https://react-icons.github.io/react-icons/ "Heading link") Libreria tiene como objetivo añadir iconos.
+- [React Icons](https://react-icons.github.io/react-icons/ "Heading link") Libreria tiene como objetivo añadir iconos, en este caso la utilize para el carrito de compras.
 - [React Bootstrap](https://react-bootstrap.github.io/ "Heading link") Libreria que utilize para el formulario de compra.
 - [Firebase](https://firebase.google.com/ "Heading link") Base de datos adonde se agregaron los productos y orders del ecommerce.
 
 ## INSTALACION
 
-- ``git clone`` <https://github.com/Diegosilcor/Diego-Silva-Cordoba-Coder-House-React-Js.git>
+- ``git clone`` <https://github.com/Diegosilcor/motors.git>
 
 - ``npm install``
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Carousel from './components/Carousel/Carousel.jsx'
+import "./App.css";
+import Navbar from "./components/NavBar/NavBar";
+import Footer from './components/Footer/Footer.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <BrowserRouter>
+          <Navbar />
+           <Carousel />
+          <Routes>
+            <Route
+              path="/category/:categoryId"
+            />
+            <Route
+              path="/detail/:productsId"
+            />
+        </Routes>
+        <Footer />
+        </BrowserRouter>
+        
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        href="https://wa.me/2348100000000"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
     </div>
   );
 }
