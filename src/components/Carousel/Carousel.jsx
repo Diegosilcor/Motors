01610406aa4ef64b85836e776ea61ff846ea83f0
Walter Franz <walter.franz@gmail.com>
2022-10-16 18:12:58 -0300

    modificaciones al carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -6,33 +6,33 @@ function CarouselIndex() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://acortar.link/HV2FAj"
+          src="images/aa2.jpeg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>Ultima tecnologia en sistemas de verificaciones de fallos</h3>
+          <h3>Revision sistemas de aire acondicionado de ultima generacion</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://acortar.link/vxBEW6"
+          src="images/Mecanica.jpeg"
           alt="Second slide"
         />
         
         <Carousel.Caption>
-          <h3>Contamos con tecnicos expertos</h3>
+          <h3>Mecanica General</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://acortar.link/rvFT2o"
+          src="images/decontaminacion.jpeg"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Inspeccion visual y computarizada del vehiculo</h3>
+          <h3>Decontaminacion y limpeiza del vehiculo</h3>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
